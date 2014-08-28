5a687c8b7309b24627534b1c4daa6f7617cb25ab
Justin Gordon <justin.gordon@gmail.com>
2014-08-27 22:40:38 -1000

    todo mvc files added, but not working

--- config/application.js
+++ config/application.js
@@ -14,7 +14,8 @@
 module.exports = function(lineman) {
   //Override application configuration here. Common examples follow in the comments.
   return {
-    loadNpmTasks: lineman.config.application.loadNpmTasks.concat("react").concat("react-bootstrap"),
+    loadNpmTasks: lineman.config.application.loadNpmTasks.concat("react").
+      concat("react-bootstrap").concat("todomvc-common").concat("director"),
     // API Proxying
     //
     // During development, you'll likely want to make XHR (AJAX) requests to an API on the same
